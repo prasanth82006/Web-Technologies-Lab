329514de8a3f908b165fbc2233b0d7d667576639
P.Prasanth kumar <166972395+prasanth82006@users.noreply.github.com>
2026-04-02 18:58:16 +0530

    Initialize App component with StudentProfile

---
+++ LAB9/Question1/src/App.js
@@ -0,0 +1,12 @@
+import React from "react";
+import StudentProfile from "./component/StudentProfile";
+
+function App() {
+  return (
+    <div>
+      <StudentProfile />
+    </div>
+  );
+}
+
+export default App;
